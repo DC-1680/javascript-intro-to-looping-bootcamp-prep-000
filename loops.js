8bfec859bc5bec8a398060f0b4f82f50852a9629
Daryl Concha <concha.daryl@gmail.com>
2018-03-15 20:46:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   var newArr = [];
-  for (var i = 0; i = 25; i++){
+  for (var i = 0; i < 25; i++){
     if(i===1){
       newArr.push("I am " + i + "strange loop.");
     } else {

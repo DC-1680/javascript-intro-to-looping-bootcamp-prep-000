0599bc40f146ca7ddefaae9d0c7266658ba74f0b
Daryl Concha <concha.daryl@gmail.com>
2018-03-15 21:03:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,15 +22,16 @@ function whileLoop(n){
 }
 
 
-/*
+
 function doWhileLoop(array){
+
   function maybeTrue() {
     return Math.random() >= 0.5
   }
 
   do{
-    array.pop;
-  } while (array.length > 0 && maybeTrue());
+    array += array.pop;
+  } while (array.length >= 0 && maybeTrue());
   return array;
 }
 
